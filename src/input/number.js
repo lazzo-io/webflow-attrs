0c82c3694290e9549c3d40b23273cbd1b49b2059
Raul Rueda <raul@raulrueda.dev>
2022-10-24 09:43:20 -0500

    Input number attribute

---
+++ src/input/number.js
@@ -0,0 +1,26 @@
+const input_number = document.querySelector('[lazzo-input-number="input"]');
+
+const input_number_btn_increase = document.querySelector(
+  '[lazzo-input-number="increase"]'
+);
+
+const input_number_btn_decrease = document.querySelector(
+  '[lazzo-input-number="decrease"]'
+);
+
+//Handle increse and decrease buttons
+input_number_btn_increase.addEventListener("click", increaseNumber);
+
+input_number_btn_decrease.addEventListener("click", decreaseNumber);
+
+function increaseNumber() {
+  if (input_number.value >= 99) return;
+
+  input_number.value = Number(input_number.value) + 1;
+}
+
+function decreaseNumber() {
+  if (input_number.value <= 1) return;
+
+  input_number.value = Number(input_number.value) - 1;
+}
