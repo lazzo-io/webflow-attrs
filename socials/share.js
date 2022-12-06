8c604bda72214fa531dae94c596147695dc69c5d
Raul Rueda <raul@raulrueda.dev>
2022-12-05 18:17:01 -0600

    Add final social attr

--- socials/share.js
+++ socials/share.js
@@ -1,20 +1,21 @@
-const social_button = document.querySelector('[lazzo-share-button]');
+const social_buttons = document.querySelector("[lazzo-share-button]");
 
-function shareButton() {
-  let social = social_button.getAttribute('lazzo-share-button');
+function shareButton(event) {
+  let social = event.getAttribute("lazzo-share-button");
 
-  var sb_text = document.title;
-  var sb_url = window.location.href;
-  var sb_link = window.location.href;
+  let sb_text = document.title;
+  let sb_url = window.location.href;
+  let sb_link = window.location.href;
 
   if (social === "facebook") sb_link = "https://www.facebook.com/sharer/sharer.php?u=" + sb_url;
 
-  if (social === "twitter")  sb_link = "https://twitter.com/intent/tweet?text=" + sb_text + "&url=" + sb_url;
+  if (social === "twitter") sb_link = "https://twitter.com/intent/tweet?text=" + sb_text + "&url=" + sb_url;
 
-  if (social === "linkedin")  sb_link = "http://www.linkedin.com/shareArticle?mini=true&title=" + sb_text + "&url=" + sb_url;
+  if (social === "linkedin") sb_link = "http://www.linkedin.com/shareArticle?mini=true&title=" + sb_text + "&url=" + sb_url;
 
-  //window.open(encodeURI(sb_link), '_blank');
   window.open(encodeURI(sb_link));
 }
 
-social_button.addEventListener("click", shareButton);
+selects.forEach((sb) => {
+  sb.addEventListener("click", shareButton);
+});
