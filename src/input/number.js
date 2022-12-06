0b731c1b1f915a7692b87460417bde07460a3f01
Raul Rueda <raul@raulrueda.dev>
2022-12-05 20:34:46 -0600

    Add new series of input
    * Input, number for increase and decrease buttons

--- src/input/number.js
+++ src/input/number.js
@@ -1,17 +1,8 @@
 const input_number = document.querySelector('[lazzo-input-number="input"]');
 
-const input_number_btn_increase = document.querySelector(
-  '[lazzo-input-number="increase"]'
-);
+const input_number_btn_increase = document.querySelector('[lazzo-input-number="increase"]');
 
-const input_number_btn_decrease = document.querySelector(
-  '[lazzo-input-number="decrease"]'
-);
-
-//Handle increse and decrease buttons
-input_number_btn_increase.addEventListener("click", increaseNumber);
-
-input_number_btn_decrease.addEventListener("click", decreaseNumber);
+const input_number_btn_decrease = document.querySelector('[lazzo-input-number="decrease"]');
 
 function increaseNumber() {
   if (input_number.value >= 99) return;
@@ -24,3 +15,8 @@ function decreaseNumber() {
 
   input_number.value = Number(input_number.value) - 1;
 }
+
+//Handle increse and decrease buttons
+input_number_btn_increase.addEventListener("click", increaseNumber);
+
+input_number_btn_decrease.addEventListener("click", decreaseNumber);
