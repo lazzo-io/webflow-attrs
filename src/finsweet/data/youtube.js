4f8a242638ae0af10a65938d6ac54ef9f980e5a9
Raul Rueda <raul@lazzo.io>
2023-03-08 03:01:49 -0600

    Update youtube.js

--- src/finsweet/data/youtube.js
+++ src/finsweet/data/youtube.js
@@ -1,4 +1,2 @@
-//name="youtube" api-key="AIzaSyDTA2LOjvM_NFeTd-F9HVQyLyCdhF9cSTI" channel-id="PLMVvpaAerL6P4iCVpzrDmnR6fUTuMYqQ_" lenght="7"
-
 const social_buttons = document.querySelectorAll("[lazzo-share-button]");
 const clipboard_buttons = document.querySelectorAll("[lazzo-share-clipboard]");
