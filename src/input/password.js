7ef402f2f3a768de995ff0f3cb18b0751833bb94
Raul Rueda <raulruedabarajas@gmail.com>
2023-09-26 02:20:37 -0600

    Password input

---
+++ src/input/password.js
@@ -0,0 +1,12 @@
+const input_b_password = document.querySelector('[lazzo-input-password="input"]');
+const input_c_password = document.querySelector('[lazzo-input-confirm-password="input"]');
+
+const input_password_btn = document.querySelector('[lazzo-input-password="button"]');
+
+input_c_password.addEventListener("input", function () {
+    input_password_btn.classList.add("disabled")
+
+    if(input_c_password.value === input_b_password.value) {
+        input_password_btn.classList.remove("disabled")
+    }
+});
